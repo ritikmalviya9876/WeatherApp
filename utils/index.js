8e5830fd08bdaf0942ce8b907610f565344d78ec
Ritik Malviya <ritikmalviya9876@gmail.com>
2021-07-09 10:57:35 +0530

    Project Commit

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+// export const colors = {
+//     PRIMARY_COLOR: '#ff304f',
+//     SECONDARY_COLOR: '#002651',
+//     BORDER_COLOR: '#dbdbdb',
+// }
+
+export const colors = {
+  PRIMARY_COLOR: "#ffba08",
+  SECONDARY_COLOR: "#6a4c93",
+  BORDER_COLOR: "#d00000",
+};
